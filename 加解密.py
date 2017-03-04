7e7fc68fa6f2cc8792afb833466abed0773785a1
bnpzsx <1150892585@qq.com>
2017-03-04 19:11:59 +0800

    字符串操作补充

--- 加解密.py
+++ 加解密.py
@@ -4,7 +4,25 @@ def encry(text,n ):
     for i in text:
         result=result + chr(ord(i)^n)
     return result
+
 def decry(text,n):
     # text=text[0:-1]
     return encry(text,n)
-    
+
+def cchr(text,fgf):
+    result=""
+    asc=0
+    for i in text:
+        if i!=fgf :
+            asc=asc*10+ord(i)-48
+        else:
+            # print(asc)
+            result=result+chr(asc)
+            asc=0
+    return result
+
+def  aasc(text,fgf):
+    result=""
+    for i in text:
+        result=result+str(ord(i))+fgf
+    return result
