333463d1cd5ff0f5612c309d484232ff09f0a09e
bnpzsx <1150892585@qq.com>
2017-03-08 19:51:23 +0800

    以数组形式存放键值对
    曾想用key,item[value]的格式来存，为了提高一点点效率，最后选择修改链表结构，从长远来看，多加一次判断可能不值

--- Hash table.py
+++ Hash table.py
@@ -1,47 +1,32 @@
 import LinkedList
 
-class item:
-    key=None
-    value=None
-    
-    def __init__(self,key,value):
-        self.key=key
-        self.value=value
-        print("create hash ",key,value)
-        
-    def __def__(self):
-        print("del hash ",self.key,self.value)
-        del self.key
-        del self.value
-        
 class HashTable:
-    table=[LinkedList.linklist()]*7 #创建Hash表
+    table=[LinkedList.linklist()]*100 #创建Hash表
     def __init__(self):
         print("建立Hash表")
 
     def set(self,key,value):
-        i=hash(key)% 7  #计算位置
-        d=self.table[i].find(key) #通过查找key判断是否存在
+        i=hash(key)% 100  #计算位置
+        d=self.table[i].find(key,0) #通过查找key判断是否存在
         if d==None :
-            self.table[i].append(key,item(key,value))
+            self.table[i].append([key,value]) # 数据以[key,value]的形式存于链表中
         else:
-            d.next.data.value=value #每个item正好存在key的后面
+            d.next.data[1]=value 
         
     def get(self,key):
-        i=hash(key)% 7  
-        d=self.table[i].find(key)
+        i=hash(key)% 100  
+        d=self.table[i].find(key,0)
         if d==None :
             return False
         else:
-            return d.next.data.value  #感觉直接把value存在data里就好了
+            return d.data[1]
 
     def hdel(self,key):
-        i=hash(key)% 7  
-        d=self.table[i].find(key)
+        i=hash(key)% 100  
+        d=self.table[i].find(key,0)
         if d==None :
             return False
         else:
-            self.table[i].remove(d.next.data) #效率低了
-            self.table[i].remove(d.data)
+            self.table[i].remove(d,True)
             return True
     
