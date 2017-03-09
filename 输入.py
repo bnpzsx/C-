e89131c412475a044fdf5f599b21f2de67cbed14
bnpzsx <1150892585@qq.com>
2017-03-09 18:28:57 +0800

    错误处理

--- 输入.py
+++ 输入.py
@@ -1,14 +1,12 @@
 while(1):
     str = input("请输入：");
-    print ("input你输入的内容是: ", str)
     if "cmd" in str:
-        print ("cmd")
         if str[0:4]=="cmd ":
             try:
-                print("执行输入命令")
-                eval(str[4:])
-            except : #ValueError as Argument
-                print (r"发生错误: /n 退出程序")
-                break
-            else:
-                pass
+                s=eval(str[4:])
+                if s!=None:
+                    print(s)
+            except Exception as e:
+                print("发生错误：",e)
+    else:
+        print("你输入的内容是: ", str)
