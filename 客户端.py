d559b98125ebb7c021b326000387da35f0e0fea4
bnpzsx <1150892585@qq.com>
2017-03-09 21:38:48 +0800

    解决命令多空格时出错问题

--- 客户端.py
+++ 客户端.py
@@ -19,6 +19,10 @@ while turn==0:
     sleep(0.1)
 while 1:
     if turn ==0:
-        c.sendall(input(""))
-        turn=1
+        s=input("").strip() #去除字符串两端空格
+        if s=="":
+            pass
+        else:
+            turn=1
+            c.sendall(s)
     else:sleep(0.1)
